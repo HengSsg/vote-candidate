d4a6c400bac8f14652061528ea48e45ee29b519a
Ju-HyeongWoo <gch04319@naver.com>
2022-03-04 16:08:39 +0900

    v_id 해야함

--- routes/vote/vote_get.js
+++ routes/vote/vote_get.js
@@ -20,4 +20,30 @@ module.exports = async function (fastify, opts) {
   .send(result)
 }
   )
+
+
+  fastify.get('/:v_id', async function (request, reply) {
+    
+  const result = 
+  {
+    "v_id" : 3,
+    "title" : "슈퍼스타K",
+    "candidate_list" : [
+{   
+    "c_name" : "이희성"
+},{
+    "c_name" : "박형석"
+   },{
+    "c_name" : "주형우"
+      }, {
+    "c_name" : "이호용"
+         }
+]
+}
+  reply
+  .code(200)
+  .header('content-type', 'application/json')
+  .send(result)
+}
+  )
 }
