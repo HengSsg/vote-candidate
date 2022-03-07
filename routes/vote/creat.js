d019e8569069c46554404e32c58418645f796eab
HengSgg <dnflekf2748@gmail.com>
2022-03-07 11:39:01 +0900

    vote/create

--- routes/vote/creat.js
+++ routes/vote/creat.js
@@ -3,10 +3,11 @@
 module.exports = async function (fastify, opts) {
   fastify.post('/', async function (request, reply) {
         //const requestbody = await createOne(this.mongo, request.body)
-        const result = {
-           'v_id' : 4
-        }
-    
+        
+        const Candidate = this.mongo.db.collection('Vote')
+        const body = request.body
+        const result = await Candidate.insertOne(body) 
+           
         reply
         .code(200)
         .header('content-type','application/json')
