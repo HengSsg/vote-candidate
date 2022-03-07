6df05653a830e960c81579a2514b7d30caaa5d47
HengSgg <dnflekf2748@gmail.com>
2022-03-07 10:27:08 +0900

    ㅇ

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
 'use strict'
 
 require('dotenv').config()
-console.log(process.env.MONGODB_ID, process.env.MONGODB_PASSWORD)
+console.log(process.env.MONGODB_ENDPOINT) // remove this after you've confirmed it working
 
 const path = require('path')
 const AutoLoad = require('fastify-autoload')
-
+const { ObjectId } = require('fastify-mongodb')
 
 module.exports = async function (fastify, opts) {
+  // Place here your custom code!
+
+  // Do not touch the following lines
+
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
     options: Object.assign({}, opts)
   })
 
+  // This loads all plugins defined in routes
+  // define your routes in one of these
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'routes'),
     options: Object.assign({}, opts)
   })
 
-  const id = process.env.MONGODB_ID
-  const password = process.env.MONGODB_PASSWORD
+const id = process.env.MONGODB_ID
+const pw = process.env.MONGODB_PASSWORD
 
-  fastify.register(require('fastify-mongodb'), {
-    forceClose: true,
-    
-    url: 'mongodb+srv://' + id + ':' + password + '@cluster0.gcslu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-  })
+fastify.register(require('fastify-mongodb'), {
+  // force to close the mongodb connection when app stopped
+  // the default value is false
+  forceClose: true,
+  
+  url: `mongodb+srv://${id}:${pw}@cluster0.tnbnq.mongodb.net/hengsgg?retryWrites=true&w=majority`
+})
+
 }
-
 
 
--- routes/vote/read.js
+++ routes/vote/read.js
@@ -1,28 +1,33 @@
 'use strict'
 
+
+  
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     
-  const vote = this.mongo.db.collection('Vote')
-  const result = await vote.find({}).toArray()
+  const dashboard = this.mongo.db.collection('dashboard')
+  const result = await dashboard.find({}).toArray()
+  console.log('result@@@',request)
+  
   reply
-      .code(200)
-      .header('content-type', 'application/json')
-      .send(result)
+  .code(200)
+  .header('content-type', 'application/json')
+  .send(result)
 }
   )
 
   fastify.get('/:id', async function (request, reply) {
     
-    const vote = this.mongo.db.collection('Vote')
-    const id = this.mongo.ObjectId(request.params.id)
-    const result = await vote.findOne(id, vote)
+  const dashboard = this.mongo.db.collection('dashboard')
+  const id = this.mongo.ObjectId(request.params.id)
+  const result = await dashboard.findOne(id,dashboard)
+  console.log('result@@@',result)
 
-    reply
-        .code(200)
-        .header('content-type', 'application/json')
-        .send(result)
-  }
-    )
+  reply
+  .code(200)
+  .header('content-type', 'application/json')
+  .send(result)
 
+}
+  )
 }
