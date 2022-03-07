01609589724b5ed705520cbf75e2e7eb6053b6d4
Ju-HyeongWoo <gch04319@naver.com>
2022-03-07 17:05:14 +0900

    update 마저해야함

--- routes/candidate/update.js
+++ routes/candidate/update.js
@@ -1,17 +1,20 @@
-'use strict'
-
 module.exports = async function (fastify, opts) {
-
-  //사용자가 특정 투표의 특정 후보를 투표함
-  fastify.put('/:c_id', async function (request, reply){
-  
+  fastify.put('/:id', async function (request, reply){
     const Candidate = this.mongo.db.collection('Candidate')
-    const result = await Candidate.findOne(request.params.id, Candidate)
-    Candidate.replaceOne(result.candidate_list.count, result.candidate_list.count + 1)
+    const id = this.mongo.ObjectId(request.params.id)
+    const filter = {'_id' : id}
+    const count = await Candidate.findOne(filter)
+    const replacement = {'count' : parseInt(count.count) + 1}
 
-    reply
+    console.log('*******replacement++++++',replacement)
+    const result = await Candidate.findOneAndReplace(filter, replacement)
+    
+
+      reply
         .code(200)
-        .header("Content-Type", "application/json")
-        .send({"c_name":result.candidate_list.name, "count":result.candidate_list.count})
-})
+        .header('content-type','application/json')
+        .send(result)
+    
+
+  })
 }
